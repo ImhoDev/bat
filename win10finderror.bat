a463b024d17d2385a80cb1a670d93c2958e8aa7c
ImhoDev <teofilo_2011@hotmail.com>
2022-08-10 15:18:54 -0500

    Update win10finderror.bat
    add permanent sol

--- win10finderror.bat
+++ win10finderror.bat
@@ -1,2 +1,10 @@
 REM solucionar el rror de busqueda de windows 10, luego de las errores
 C:\Windows\system32\ctfmon.exe
+
+REM SOLOCUCIÓN PERMANENTE
+REM 1. Diríjanse al cuadro de ejecución (combinación de teclas Windows + R)
+REM 2. Escriban "shell:startup" (sin las comillas) y pulsen ENTER. Se abrirá una ventana.
+REM 3. En otra ventana diríjanse a C:\Windows\system32, ubiquen el archivo "ctfmon", denle un clic derecho y seleccionen "Escritorio (crear acceso directo)".
+REM 4. Diríjanse al escritorio, ubiquen el acceso directo y cámbienle el nombre para que quede solo como "ctfmon" (sin las comillas).
+REM 5. Peguen ese acceso directo en la ventana que se abrió en el paso 2.
+REM 6. Reinicien el ordenador. Eso es todo.
