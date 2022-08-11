fb50e5a21768ff78d37eaa4c1cc5b8176cfcf4c7
ImhoDev <teofilo_2011@hotmail.com>
2022-08-11 11:33:08 -0500

    Update f10.bat
    add comando convert to gpt and mbr

--- f10.bat
+++ f10.bat
@@ -6,7 +6,13 @@ list disk
 REM seleccionar disco
 select disk 0
 REM limpiar
+REM tener encuenta que se perderán todos los datos del disco con este comando
 clean
+REM por defecto el sistema de partición es MBR
+REM convertir a sistema a GPT
+convert gpt
+REM convertir a MBR
+convert mbr
 create partition primary
 create partition primary size=500000
 exit 
